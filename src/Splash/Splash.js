fb0e5023967127b7907de73c3d566a579201cbbb
Kevin Krato <kratok08@gmail.com>
2019-06-07 15:08:59 -0600

    Scaffold component

--- src/Splash/Splash.js
+++ src/Splash/Splash.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
+import "./Splash.scss";
 
-export default function Splash() {
-  return (
-    <div>
-      <h1>Splash Comp</h1>
-    </div>
-  );
+export default class Splash extends Component {
+  toDashBoard = () => {
+    this.props.history.push("/dashboard");
+  };
+  render() {
+    return (
+      <div className="Splash">
+        <header className="header-content">
+          <div className="title-card">
+            <h1>Github Explorer</h1>
+            <p>
+              Search through millions of accounts and see what stories they have
+              to share.
+            </p>
+            <button onClick={this.toDashBoard}>Start Exploring</button>
+          </div>
+        </header>
+        <section>
+          <h1>Put content here</h1>
+          <p>Something cool</p>
+        </section>
+      </div>
+    );
+  }
 }
