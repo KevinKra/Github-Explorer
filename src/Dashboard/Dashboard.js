7e5a60ef0b06c4fbc54db5e0c641c0887ff09826
Kevin Krato <kratok08@gmail.com>
2019-06-07 16:08:31 -0600

    Render UserCard components

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import * as apiCalls from "../utils/apiCalls";
+import { UserCard } from "../UserCard/UserCard";
 
 class Dashboard extends Component {
+  state = {
+    users: [],
+    loading: true
+  };
+  async componentDidMount() {
+    const users = await apiCalls.collectUsers();
+    this.setState({ users, loading: false });
+  }
+
+  renderUserCards = () => {
+    return this.state.users.map(user => <UserCard user={user} key={user.id} />);
+  };
   render() {
     return (
       <div>
+        <div>{this.renderUserCards()}</div>
         <h1>Dashboard</h1>
-        <h3>Users</h3>
       </div>
     );
   }
