eaf441f7adfedaf58c2c08a6b3ca29a306a52c53
Kevin Krato <kratok08@gmail.com>
2019-06-07 14:24:36 -0600

    Create primary components

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
+import NavBar from "../NavBar/NavBar";
+import Splash from "../Splash/Splash";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h3>Hello World</h3>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <main className="App">
+        <Route path="/" component={Splash} />
+        <Route path="/main" component={NavBar} />
+        <Route path="/main" component={Dashboard} />
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <nav>
+        <i>icon</i>
+        <h4>Title</h4>
+      </nav>
+    );
+  }
+}
