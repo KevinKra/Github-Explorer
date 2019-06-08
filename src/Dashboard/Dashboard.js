495d152e870a09ac6a68582f9576515307b35704
Kevin Krato <kratok08@gmail.com>
2019-06-08 11:40:38 -0600

    Facilitate SideBar

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -3,12 +3,15 @@ import * as apiCalls from "../utils/apiCalls";
 import { UserCard } from "../UserCard/UserCard";
 import { Spinner } from "../Spinner/Spinner";
 import NavBar from "../NavBar/NavBar";
+import SideBar from "../SideBar/SideBar";
 import "./Dashboard.scss";
 
 class Dashboard extends Component {
   state = {
     users: [],
-    loading: true
+    userData: {},
+    loading: true,
+    sideBar: false
   };
   async componentDidMount() {
     const users = await apiCalls.collectUsers("users");
@@ -21,24 +24,28 @@ class Dashboard extends Component {
     this.setState({ users: output.items, loading: false });
   };
 
+  moreInfo = data => {
+    const toggle = this.state.sideBar;
+    this.setState({ sideBar: !toggle, userData: data });
+  };
+
   renderUserCards = () => {
-    console.log(this.state.users);
     const output = this.state.users.map(user => (
-      <UserCard user={user} key={user.id} />
+      <UserCard user={user} key={user.id} moreInfo={this.moreInfo} />
     ));
     return output;
   };
 
   render() {
-    const { loading } = this.state;
+    const { loading, sideBar } = this.state;
     return (
       <Fragment>
         <NavBar searchUsers={this.searchUsers} />
         <main className="Dashboard">
-          <h3>Dashboard</h3>
           <section className="user-cards">
             {loading ? <Spinner /> : this.renderUserCards()}
           </section>
+          <SideBar sideBar={sideBar} />
         </main>
       </Fragment>
     );
